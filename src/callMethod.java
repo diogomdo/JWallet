e7a79db1ec8d26661ee4c771601a278df7bb77ff
diogomdo <diogomdo@gmail.com>
2014-06-15 01:38:02 +0100

    first commit

---
+++ src/callMethod.java
@@ -0,0 +1,6 @@
+
+public interface callMethod {
+
+	public void execute();
+
+}
---
+++ src/wallet.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+public class wallet {
+	
+	public static void main(String[] args) {
+		
+		String command = null;
+		
+		//http://codereview.stackexchange.com/a/27505
+		Map<String, callMethod> commandList = new HashMap<>();
+		
+		commandList.put("add", new callMethod() {
+		    public void execute() {
+		    	addOperation();
+		    }});
+
+		System.out.println("Starting JWallet");
+		
+		final callMethod serviceMethod = commandList.get("add");
+		serviceMethod.execute();
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter command:");
+		command = input.nextLine();
+		
+		System.out.println("Command inserted: "+command);
+			
+	}
+	
+	static void addOperation() { 
+		System.out.println("ADD");
+	}
+
+
+}
